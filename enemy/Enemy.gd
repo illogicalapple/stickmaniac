89f8c57a16143cd2186d83dc1bdfe632a7f73c3f
StormLight14 <stormpower44@protonmail.com>
2022-11-18 20:31:56 -0700

    update Enemy.gd
    remove unnesararary 2 lines of code

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -3,10 +3,8 @@ extends KinematicBody2D
 onready var health_bar = $HealthBar
 
 var health = 100
-var max_health = 0
 
 func _ready():
-	var max_health = health
 	health_bar.max_value = health
 
 func _on_Hurtbox_area_entered(area):
--- stickmin/Stickmin.gd
+++ stickmin/Stickmin.gd
@@ -6,5 +6,5 @@ func _process(_delta):
 	pass
 
 func _on_Hurtbox_area_entered(body):
+	print("hurt")
 	emit_signal("damage", 10) # change damage amount later
-	print("ping")
