d70d3c1fc95dd82030138665d360dbf0ff1441da
StormLight14 <stormpower44@protonmail.com>
2022-11-19 12:41:05 -0700

    add music icon toggle

--- sound/SoundPlayer.gd
+++ sound/SoundPlayer.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var root = get_tree().current_scene
+onready var music_button = root.get_node("GUI/MusicToggled")
 
 var music_enabled = false
 
@@ -10,6 +11,9 @@ func _ready():
 func _on_Music_toggled():
 	if music_enabled:
 		music_enabled = false
+		music_button.icon.current_frame = 0
 	else:
 		music_enabled = true
+		music_button.icon.current_frame = 1
+	
 	$Music.playing = music_enabled
