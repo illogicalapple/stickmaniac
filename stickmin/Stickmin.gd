e5d378af11d8b1ebab7c014980b6c237a027a87f
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-11-19 12:35:30 -0600

    tested things
    (didn't work)

--- stickmin/Stickmin.gd
+++ stickmin/Stickmin.gd
@@ -1,10 +1,19 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 signal damage(amount)
 
-func _process(_delta):
-	emit_signal("damage", 0.1)
+# func _process(_delta):
+#	emit_signal("damage", 0.1) (was for testing)
 
-func _on_Hurtbox_area_entered(body):
+func _ready():
+	set_use_custom_integrator(true)
+	print("ready")
+
+func _on_Hurtbox_area_entered(_body):
 	print("hurt")
 	emit_signal("damage", 10) # change damage amount later
+
+
+func _on_Hurtbox_mouse_entered():
+	emit_signal("damage", 20) # testing
+	print("mouse entered")
