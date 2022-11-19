6b54f4f9ec7015477bf661a76b6369852f669362
StormLight14 <stormpower44@protonmail.com>
2022-11-18 20:13:59 -0700

    Update Enemy.gd
    only show health bar after hit

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -3,8 +3,10 @@ extends KinematicBody2D
 onready var health_bar = $HealthBar
 
 var health = 100
+var max_health = 0
 
 func _ready():
+	var max_health = health
 	health_bar.max_value = health
 
 func _on_Hurtbox_area_entered(area):
@@ -12,3 +14,5 @@ func _on_Hurtbox_area_entered(area):
 	health_bar.value = health
 	if health <= 0:
 		queue_free()
+		
+	health_bar.visible = true
