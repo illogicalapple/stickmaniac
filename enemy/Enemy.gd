d979c92cee007dfd6c247b3568aeb506801f68b1
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-11-20 11:09:55 -0600

    enemy stuff
    nerf enemy life; change to low processor mode, add spawn button

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -1,16 +1,23 @@
 extends KinematicBody2D
 
+signal coins(amount)
+
 onready var health_bar = $HealthBar
 
-var health = 100
+var health = 25
+var value = 43 # todo: make it random
 
 func _ready():
 	health_bar.max_value = health
+
+func suicide_bomb():
+	emit_signal("coins", value)
+	# drop loot
+	queue_free()
 
 func _on_Hurtbox_area_entered(area):
 	health -= area.damage
 	health_bar.value = health
 	if health <= 0:
-		queue_free()
-		
+		suicide_bomb()
 	health_bar.visible = true
--- ui/GUI.gd
+++ ui/GUI.gd
@@ -3,6 +3,7 @@ extends Control
 var enemy = preload("res://enemy/Enemy.tscn") # DEBUG, ADD TO SPAWNER
 var rng = RandomNumberGenerator.new() # DEBUG, ADD TO SPAWNER
 var screen_size # DEBUG, ADD TO SPAWNER
+var score = 0
 
 onready var root = get_tree().current_scene
 
@@ -18,12 +19,17 @@ func _on_damage(amount):
 
 func _on_MusicToggled_pressed():
 	emit_signal("music_toggled")
-	
+
+func add_coins(amount):
+	score += amount
+	$Score.text = "score: " + str(score)
+
 func spawn_enemy(): # DEBUG, ADD TO SPAWNER
 	var enemy_size = Vector2(100, 100) # DEBUG, ADD TO SPAWNER
 	var enemy_instance = enemy.instance() # DEBUG, ADD TO SPAWNER
 	enemy_instance.position = Vector2(rng.randf_range(enemy_size.x / 2, screen_size.x - enemy_size.x / 2), rng.randf_range(enemy_size.y / 2 + 30, screen_size.y - enemy_size.y / 2)) # DEBUG, ADD TO SPAWNER
 	get_tree().get_root().add_child(enemy_instance) # DEBUG, ADD TO SPAWNER
+	enemy_instance.connect("coins", self, "add_coins")
 
 func _on_SpawnEnemy_pressed(): # DEBUG
 	spawn_enemy() # DEBUG
