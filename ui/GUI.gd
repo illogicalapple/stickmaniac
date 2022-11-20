97decce8c0eb3f2d4f9fa6196965c0ba99fbe0a1
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-11-20 10:54:59 -0600

    add enemy spawning
    spawn a few enemies (stormlight look at your dms)

--- ui/GUI.gd
+++ ui/GUI.gd
@@ -1,10 +1,16 @@
 extends Control
 
+var enemy = preload("res://enemy/Enemy.tscn") # DEBUG, ADD TO SPAWNER
+var rng = RandomNumberGenerator.new() # DEBUG, ADD TO SPAWNER
+var screen_size # DEBUG, ADD TO SPAWNER
+
 onready var root = get_tree().current_scene
 
 signal music_toggled
 
 func _ready():
+	screen_size = get_viewport_rect().size # DEBUG, ADD TO SPAWNER
+	rng.randomize() # DEBUG, ADD TO SPAWNER
 	root.get_node("Stickmin").connect("damage", self, "_on_damage")
 
 func _on_damage(amount):
@@ -12,3 +18,12 @@ func _on_damage(amount):
 
 func _on_MusicToggled_pressed():
 	emit_signal("music_toggled")
+	
+func spawn_enemy(): # DEBUG, ADD TO SPAWNER
+	var enemy_size = Vector2(100, 100) # DEBUG, ADD TO SPAWNER
+	var enemy_instance = enemy.instance() # DEBUG, ADD TO SPAWNER
+	enemy_instance.position = Vector2(rng.randf_range(enemy_size.x / 2, screen_size.x - enemy_size.x / 2), rng.randf_range(enemy_size.y / 2 + 30, screen_size.y - enemy_size.y / 2)) # DEBUG, ADD TO SPAWNER
+	get_tree().get_root().add_child(enemy_instance) # DEBUG, ADD TO SPAWNER
+
+func _on_SpawnEnemy_pressed(): # DEBUG
+	spawn_enemy() # DEBUG
