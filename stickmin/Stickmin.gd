b520922e39d32ddb54c49452043b4eae4961b5bc
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-11-20 11:39:16 -0600

    Revert "will revert"
    This reverts commit bba59557b2805579e7b8baa5686164cab5c2dee9.

--- stickmin/Stickmin.gd
+++ stickmin/Stickmin.gd
@@ -1,8 +1,6 @@
-extends Area2D
+extends KinematicBody2D
 
 signal damage(amount)
-
-var dps = 0
 
 # func _process(_delta):
 #	emit_signal("damage", 0.1) (was for testing)
@@ -11,5 +9,10 @@ func _ready():
 	#set_use_custom_integrator(true)
 	print("ready")
 
-func _on_Stickmin_body_entered(body):
-	emit_signal("damage", 20)
+func _on_Hurtbox_area_entered(_body):
+	print("hurt")
+	emit_signal("damage", 10) # change damage amount later
+
+func _on_Hurtbox_mouse_entered():
+	emit_signal("damage", 20) # testing
+	print("mouse entered")
