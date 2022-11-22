9b95d1f05be4d7240ba84d38f838d2d7eea2cb64
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-11-21 19:51:36 -0600

    health bar improvements
    change color when you have less health

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -3,10 +3,12 @@ extends KinematicBody2D
 signal coins(amount)
 
 onready var health_bar = $HealthBar
+onready var foreground = health_bar.get("custom_styles/fg")
 
 var drop = preload("drops/laser/Laser.tscn")
 
-var health = 25
+var max_health = 25
+var health = max_health
 var value = 43 # todo: make it random
 var height = 100
 
@@ -27,4 +29,8 @@ func _on_Hurtbox_area_entered(area):
 	health_bar.value = health
 	if health <= 0:
 		suicide_bomb()
+	elif health <= 0.33 * max_health:
+		foreground.bg_color = Color("ff5f5f")
+	elif health <= 0.66 * max_health:
+		foreground.bg_color = Color("ffdc3b")
 	health_bar.visible = true
--- ui/GUI.gd
+++ ui/GUI.gd
@@ -1,11 +1,17 @@
 extends Control
 
+# healthbar colors:
+# high - 56c24a
+# medium - ffdc3b
+# low - ff5f5f
+
 var enemy = preload("res://enemy/Enemy.tscn") # DEBUG, ADD TO SPAWNER
 var rng = RandomNumberGenerator.new() # DEBUG, ADD TO SPAWNER
 var screen_size # DEBUG, ADD TO SPAWNER
 var score = 0
 
 onready var root = get_tree().current_scene
+onready var foreground = $HealthBar.get("custom_styles/fg")
 
 signal music_toggled
 
@@ -16,6 +22,10 @@ func _ready():
 
 func _on_damage(amount):
 	$HealthBar.value -= amount
+	if $HealthBar.value <= 33:
+		foreground.bg_color = Color("ff5f5f")
+	elif $HealthBar.value <= 66:
+		foreground.bg_color = Color("ffdc3b")
 
 func _on_MusicToggled_pressed():
 	emit_signal("music_toggled")
