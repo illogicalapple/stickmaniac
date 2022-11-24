61303f5b6b5e88a02871d15406511355f9faa5d6
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2022-11-24 13:56:28 -0600

    add reset button

--- death_screen/DeathScreen.gd
+++ death_screen/DeathScreen.gd
@@ -3,3 +3,7 @@ extends Node
 func _ready():
 	if global.music_enabled:
 		$Music.play(global.song_time * 3/5)
+
+func _input(event):
+	if event.is_action_pressed("ui_pause"):
+		get_tree().change_scene("res://Root.tscn")
