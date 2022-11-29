ac609e5086f8bf1eee3546894ebbf84a1815eb83
CatCatDeluxe <oliver.f.lepp@gmail.com>
2022-11-28 21:36:25 -0500

    Remove some framerate dependency + general cleanup of Head.gd. Not removing the framecap yet though, still needs more testing

--- stickmin/Head.gd
+++ stickmin/Head.gd
@@ -1,19 +1,19 @@
 extends Sprite
 
-var viewport_size
-var velocity = Vector2(0, 0)
-var id = 0
+var viewport_size: Vector2
+var velocity := Vector2(0, 0)
+var id: int = 0
 
-func is_pressed(action):
+func is_pressed_float(action: String) -> float:
 	if Input.is_action_pressed(action):
-		return 1
+		return 1.0
 	else:
-		return 0
+		return 0.0
 
 func _ready():
 	viewport_size = get_viewport_rect().size
 	if global.singleplayer:
-		id = ""
+		id = 0
 		position.x = viewport_size.x / 3
 		position.y = viewport_size.y / 2
 		$Label.queue_free()
@@ -30,13 +30,17 @@ func _ready():
 			3:
 				$Label.add_color_override("font_color", Color("ffdc3b"))
 
-func _process(_delta):
-	velocity.x += is_pressed("ui_right" + str(id)) - is_pressed("ui_left" + str(id))
-	velocity.y += is_pressed("ui_down" + str(id)) - is_pressed("ui_up" + str(id))
-	position += velocity
-	$Body.head_velocity = velocity
+func _process(delta: float):
+	var body: Node = get_node("Body")
+
+	velocity.x += is_pressed_float("ui_right" + str(id)) - is_pressed_float("ui_left" + str(id))
+	velocity.y += is_pressed_float("ui_down" + str(id)) - is_pressed_float("ui_up" + str(id))
+	position += velocity * delta * 60.0
+	body.head_velocity = velocity
+
 	velocity *= 0.88
+
 	if velocity.x < 0:
-		$Body.gun_arm = 0
+		body.gun_arm = 0
 	else:
-		$Body.gun_arm = 1
+		body.gun_arm = 1
